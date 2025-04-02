0180da09d29cb5405770422d80228b60633c2a45
supremhum <kalystols@gmail.com>
2025-04-02 13:51:10 +0900

    refactor: MemberUpdateById PUT Method

--- src/main/java/com/example/schedulev2/service/MemberServiceImpl.java
+++ src/main/java/com/example/schedulev2/service/MemberServiceImpl.java
@@ -13,6 +13,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import jakarta.persistence.criteria.Predicate;
 import java.util.stream.Collectors;
@@ -24,10 +25,13 @@ public class MemberServiceImpl implements MemberService{
     private final MemberRepository memberRepository;
 
     @Override
-    @Transactional
+    @Transactional // .save() 안에 Transactional 이 있어서 딱히 필요는 없지만 일단 명시
     public SignUpResponseDto signUp(SignUpRequestDto requestDto) {
 
         Member member = new Member(requestDto.getEmail(),requestDto.getPassword(),requestDto.getName());
+        if(memberRepository.findMemberByEmail(requestDto.getEmail()).isPresent()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"이미 존재하는 이메일 입니다");
+        }
         Member saveMember = memberRepository.save(member);
         return new SignUpResponseDto(saveMember.getId(), saveMember.getEmail(), saveMember.getName());
     }
@@ -68,6 +72,8 @@ public class MemberServiceImpl implements MemberService{
         return new MemberByIdResponseDto(member.getEmail(), member.getName(),member.getCreatedAt(),member.getModifiedAt());
     }
 
+    // 트러블슈팅 발생
+    // 자바의 대소문자 구분과 DB에서 찾는건 대소문자 구분없이 하는것의 차이에 의해 발생한 사건
     @Transactional
     @Override
     public MemberResponseDto updateById(Long id, MemberUpdateRequestDto requestDto) {
@@ -75,7 +81,23 @@ public class MemberServiceImpl implements MemberService{
         if (!findMember.getPassword().equals(requestDto.getPassword())) {
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED,"Password does not match");
         }
+
+        // 이메일을 찾는다 일단 getEmail이 null 이 아닌 경우에서만.
+        // 애초에 이메일이 기존 이메일이랑 같으면 넘어가고 아니면
+        if (requestDto.getEmail()!=null && !Objects.equals(requestDto.getEmail(),findMember.getEmail())) {
+            // 요까진 입력된 이메일이 기존 이메일과 다른 경우다.
+
+            Optional<Member> memberByEmail = memberRepository.findMemberByEmail(requestDto.getEmail());
+
+            // 이제 dto 이메일을 가진 데이터를 찾고 없으면 넘기고 있으면 예외
+            // 추가로 &&이후 찾은 데이터의 id가 기존id와 같으면 탈출
+            if (memberByEmail.isPresent()&&!memberByEmail.get().getId().equals(id)){
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "이미 존재하는 이메일 입니다");
+            }
+        }
+
         findMember.updateNameOrEmail(requestDto.getName(),requestDto.getEmail());
+
         return new MemberResponseDto(findMember);
     }
 
